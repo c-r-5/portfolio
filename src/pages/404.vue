56c5fca5cbcd4d8079e33082c0370819ed9ad176
c_r_5 <48353236+c-r-5@users.noreply.github.com>
2026-02-24 13:26:35 +0900

    Add 404 page not found component

---
+++ src/pages/404.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-container class="py-8" max-width="1000">
+    <h1 class="text-h4 font-weight-bold mb-4">404 - Page Not Found</h1>
+    <p>ページが見つかりませんでした。</p>
+  </v-container>
+</template>
+
+<script setup>
+// Contact page
+</script>
